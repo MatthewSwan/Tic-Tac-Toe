c93171fe19257e9beae95bdf7c6eae67d3b02a28
Vince Lundgren <vincent.lundgren@gmail.com>
2016-04-14 16:16:56 -0500

    added working oauth and test to verify user stays logged in until he/she logged out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,12 @@
 class SessionsController < ApplicationController
   def create
-    User.create(uid: env['omniauth.auth']['uid'], name: env['omniauth.auth']['info']['name'])
+    user = User.find_or_create_by(uid: env['omniauth.auth']['uid'], name: env['omniauth.auth']['info']['name'])
+    session[:user_id] = user.id
+    redirect_to root_path
+  end
+
+  def destroy
+    session.destroy
     redirect_to root_path
   end
 end
